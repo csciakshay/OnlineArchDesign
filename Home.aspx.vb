038fb039a445892143ee9fc53394b383b246e1e1
Akshay Darji <Y04055@ad.csci.co.in>
2020-03-16 09:06:30 +0530

    dummy DB changes commit

--- Home.aspx.vb
+++ Home.aspx.vb
@@ -4,15 +4,15 @@ Partial Class Home
 
     Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
         If Not Page.IsPostBack Then
-            If Not Session("uname") Is Nothing Then
+            '  If Not Session("uname") Is Nothing Then
 
-                SqlDataSource1.SelectCommand = "select a.*, (SELECT   TOP (1) Data FROM designImages WHERE a.id = id) AS images from ardesignMaster a"
+            SqlDataSource1.SelectCommand = "select a.*, (SELECT   TOP (1) Data FROM designImages WHERE a.id = id) AS images from ardesignMaster a"
 
-                SqlDataSource1.DataBind()
-                DataList1.DataBind()
-            Else
+            SqlDataSource1.DataBind()
+            DataList1.DataBind()
+            'Else
 
-            End If
+            '  End If
 
             ' SqlDataSource3.SelectCommand = "select TOP (5) a.*,b.name, (SELECT   TOP (1) images FROM propertyImages WHERE a.id = propertyid) AS images from PropertyMaster a left join UserMaster b on a.userid=b.id WHERE propertysold = 'N' order by views desc "
 
